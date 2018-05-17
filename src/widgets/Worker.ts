b6010c387d4a4640b221b586f7c0df2716440077
suren <p.surendrakumar@gmail.com>
2018-05-18 01:06:26 +0530

    adding css to Workers

--- src/widgets/Worker.ts
+++ src/widgets/Worker.ts
@@ -1,5 +1,13 @@
 import { WidgetBase } from '@dojo/widget-core/WidgetBase';
 import { v } from '@dojo/widget-core/d';
+// we will apply a theme mixin to the Worker widget. 
+// A mixin is not intended to be used on its own, but instead works with a class to add useful functionality.
+import { theme, ThemedMixin } from '@dojo/widget-core/mixins/Themed';
+import * as css from '../styles/worker.m.css';
+
+
+// First, apply a decorator to the class to modify the widget’s constructor 
+// and prepare its instances to work with CSS modules
 
 export default class Worker extends WidgetBase<WorkerProperties> {
 	protected render() {
